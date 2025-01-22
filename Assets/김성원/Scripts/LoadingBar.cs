b28dcb39d47331456fc09b02679b6f90310e398e
Kim Seong Won <worni5015@gmail.com>
2025-01-22 20:26:52 +0900

    Loading화면 구현

---
+++ Assets/김성원/Scripts/LoadingBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingBar : MonoBehaviour
+{
+    public RectTransform front;
+    float time = 0;
+
+    private void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+    private void Update()
+    {
+        time += Time.deltaTime;
+        front.localScale = new Vector3(time / 3.0f, 1.0f, 1.0f);
+        if (time >= 3.0f)
+        {
+            front.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            Invoke("StartGame", 0.5f);
+        }
+    }
+    public void StartGame()
+    {
+        //씬 불러오기
+        SceneManager.LoadScene(/*불러올 씬 파일 이름*/"SampleScene");
+    }
+}
