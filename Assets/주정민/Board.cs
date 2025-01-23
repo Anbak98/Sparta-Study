b76b9b601d736fc5a95993a1cd51b72e423eab01
Jujeongmin <anjshdkdl99@gmail.com>
2025-01-23 17:31:20 +0900

    JJMBoardSuJeong

--- Assets/주정민/Board.cs
+++ Assets/주정민/Board.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Board : MonoBehaviour
 {
     [SerializeField] GameObject bod;
+    [SerializeField] Camera camera;
 
     // Start is called before the first frame update
     void Start()
@@ -13,16 +15,34 @@ public class Board : MonoBehaviour
         int height = PlayerPrefs.GetInt("Height");
         float interval = PlayerPrefs.GetFloat("Interval");
 
+        if (width == 6)
+        {
+            camera.transform.position = new Vector3(2.47f, 3.38f, -8.98f);
             for (int i = 0; i < width * height; ++i)
             {
+
                 GameObject go = Instantiate(bod, this.transform);
 
-            float x = (i % width) * interval;
-            float y = (i / height) * interval;
+                float x = (i % width) * interval;
+                float y = (i / height) * interval;
 
                 go.transform.position = new Vector2(x, y);
             }
-        
+        }
+        else if (width == 10)
+        {
+            camera.transform.position = new Vector3(3.58f, 4.83f, -12.2f);
+            for (int i = 0; i < width * height; ++i)
+            {
+
+                GameObject go = Instantiate(bod, this.transform);
+
+                float x = (i % width) * interval;
+                float y = (i / height) * interval;
+
+                go.transform.position = new Vector2(x, y);
+            }
+        }
     }
 
     // Update is called once per frame
