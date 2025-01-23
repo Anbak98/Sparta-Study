47de92858a9c60078cbe2448fbad3cc786b6f562
Kim Seong Won <worni5015@gmail.com>
2025-01-23 16:09:35 +0900

    GameManager (시간구현, 성공시 성공패널, 실패시 실패패널 기능 구현)

---
+++ Assets/김성원/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Card firstCard;
+    public Card secondCard;
+
+    public Text timeTxt;
+    public GameObject SuccessEndPanel;
+    public GameObject FailEndPanel;
+
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    public int cardCount = 0;
+    float time = 0.0f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        FailEndPanel.SetActive(false);
+        SuccessEndPanel.SetActive(true);
+        Application.targetFrameRate = 60;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        time = PlayerPrefs.GetFloat("Time");
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (cardCount!= 0 && time <= 0.0f)
+        {
+            Time.timeScale = 0.0f;
+            timeTxt.text = "0.0";
+            FailEndPanel.SetActive(true);
+        }
+
+        timeTxt.text = time.ToString("N2");
+    }
+    public void Matched()
+    {
+        if (firstCard.idx == secondCard.idx)
+        {
+
+            //ÆÄ±«ÇØ¶ó
+            audioSource.PlayOneShot(clip);
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+            cardCount -= 2;
+            if (cardCount == 0)
+            {
+                Time.timeScale = 0.0f;
+                SuccessEndPanel.SetActive(true);
+                timeTxt.text = time.ToString("N2");
+            }
+        }
+        else
+        {
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+        firstCard = null;
+        secondCard = null;
+    }
+}
