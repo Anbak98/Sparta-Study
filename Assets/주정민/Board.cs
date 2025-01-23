c7365d1604b525028a73c978f8066a0b4e519d52
Jujeongmin <anjshdkdl99@gmail.com>
2025-01-23 15:59:41 +0900

    BoardScene

---
+++ Assets/주정민/Board.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] GameObject bod;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int width = PlayerPrefs.GetInt("Width");
+        int height = PlayerPrefs.GetInt("Height");
+        float interval = PlayerPrefs.GetFloat("Intertval");
+
+            for (int i = 0; i < width * height; ++i)
+            {
+                GameObject go = Instantiate(bod, this.transform);
+
+            float x = (i % width) * interval;
+            float y = (i / height) * interval;
+
+                go.transform.position = new Vector2(x, y);
+            }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
